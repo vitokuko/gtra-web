26cc6bd3514b549e06eaf4c20e05fabb2c5b0af2
elisadiouf <dioufelisabeth98@gmail.com>
2020-11-28 02:41:46 +0100

    integration page d'authentification

---
+++ src/app/auth/page-mot-de-passe-oublier/page-mot-de-passe-oublier.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-mot-de-passe-oublier',
+  templateUrl: './page-mot-de-passe-oublier.component.html',
+  styleUrls: ['./page-mot-de-passe-oublier.component.css']
+})
+export class PageMotDePasseOublierComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
