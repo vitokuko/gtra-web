44e6a44de6e6d8c519cb54198617eb47f9cebd35
souaibou <ndiayevito04@gmail.com>
2020-12-05 23:31:46 +0000

    add function connect actually for redirection to page home

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {DataService} from "../../shared/data/data.service";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router, public dataService: DataService) { }
 
   ngOnInit(): void {
+  }
+
+  connect(): void {
+    this.router.navigate(['/home/accueil']);
   }
 
 }
