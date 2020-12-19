c85ccc1be3e1d4c03eeafcb2d1ce18bdaf1146d1
elisadiouf <dioufelisabeth98@gmail.com>
2020-12-19 23:06:21 +0100

    get list clients admin

--- src/app/home/client/client.component.ts
+++ src/app/home/client/client.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Utilisateur } from 'src/app/models/utilisateur';
+import {DataService} from '../../shared/data/data.service';
 
 @Component({
   selector: 'app-client',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientComponent implements OnInit {
 
-  constructor() { }
+  title = 'Listes des clients';
+  allclients: Utilisateur[]= [];
+
+  client: Utilisateur = new Utilisateur();
+
+  id = typeof this.client=== 'number' ? this.client : this.client.id;
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.getAllClients();
+  }
+
+  getAllClients(): void {
+    this.dataService.get('Utilisateurs')
+      .then(
+        (res: any) => {
+          console.log(res);
+          this.allclients = res;
+        },
+        err => {
+          console.log(err);
+        }
+      );
   }
 
 }
--- src/app/home/gestion-clients/gestion-clients.component.ts
+++ src/app/home/gestion-clients/gestion-clients.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Utilisateur } from 'src/app/models/utilisateur';
+import {DataService} from '../../shared/data/data.service';
 
 @Component({
   selector: 'app-gestion-clients',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gestion-clients.component.css']
 })
 export class GestionClientsComponent implements OnInit {
+  title = 'Listes des clients';
+  allclients: Utilisateur[]= [];
 
-  constructor() { }
+  client: Utilisateur = new Utilisateur();
+
+  id = typeof this.client=== 'number' ? this.client : this.client.id;
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.getAllClients();
+  }
+
+  getAllClients(): void {
+    this.dataService.get('Utilisateurs')
+      .then(
+        (res: any) => {
+          console.log(res);
+          this.allclients = res;
+        },
+        err => {
+          console.log(err);
+        }
+      );
   }
 
 }
